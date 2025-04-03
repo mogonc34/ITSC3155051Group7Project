128c61d4f4c7d9b4b9d8340c73b81464abce87fa
Dpruitt5 <davidwpruitt@gmail.com>
2025-04-03 12:03:05 -0400

    David put his card and added the mood board and image

--- group_bio_template/script.js
+++ group_bio_template/script.js
@@ -11,6 +11,15 @@ function toggleBio(bioId) {
         bio.style.display = "none";
     }
 }
+function toggleMoodboard(moodboard) {
+    const bio = document.getElementById(moodboard);
+    // Toggle between showing and hiding the bio section
+    if (bio.style.display === "none" || bio.style.display === "") {
+        bio.style.display = "block";
+    } else {
+        bio.style.display = "none";
+    }
+}
 
 /**
  * Shows the specified section ('bios' or 'vision') and hides the other

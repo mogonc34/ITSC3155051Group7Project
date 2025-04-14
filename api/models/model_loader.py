da3251afe9e118f3bf2915e4fc73159382ad7418
Michael Goins <mogoins@charlotte.edu>
2025-04-13 20:02:36 -0400

    doc: updated config.py comments

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,6 +1,8 @@
 # from Code Skeleton
 
-from . import orders, order_details, recipes, sandwiches, resources
+from api.models import order_promotion, payment_model
+from . import customer, ingredient, menu_item, menu_item_ingredients, 
+    order, order_item, order_promotion, payment, payment_model, promotions, rating_review
 
 from ..dependencies.database import engine
 
@@ -8,6 +10,13 @@ from ..dependencies.database import engine
 def index():
     orders.Base.metadata.create_all(engine)
     order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
-    resources.Base.metadata.create_all(engine)
+    customer.Base.metadata.create_all(engine)
+    ingredient.Base.metadata.create_all(engine)
+    menu_item.Base.metadata.create_all(engine)
+    menu_item_ingredients.Base.metadata.create_all(engine)
+    order.Base.metadata.create_all(engine)
+    order_promotion.Base.metadata.create_all(engine)
+    payment.Base.metadata.create_all(engine)
+    payment_model.Base.metadata.create_all(engine)
+    promotions.Base.metadata.create_all(engine)
+    rating_review.Base.metadata.create_all(engine)
